54f9ec7ad440b0ea1bf8e5f6c8b366871f60c285
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-27 10:29:04 -0700

    First commit

---
+++ StartingPoint/Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Alex Shillingford on 1/27/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
